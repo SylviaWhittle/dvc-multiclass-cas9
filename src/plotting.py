3587b17491a842cb0e04d5ed0821ade5aa9b57bb
SylviaWhittle <sylviwhittle@gmail.com>
2025-11-20 15:35:17 +0000

    GOOD MODEL? saving these params for later

--- src/plotting.py
+++ src/plotting.py
@@ -19,11 +19,15 @@ def plot_image_mask_prediction(
     num_output_channels = mask.shape[2]
     num_input_channels = image.shape[2]
     # Total plots = number input channels + number output channels (for GT) + number output channels
-    # (predicted) *2 (for raw and thresholded)
-    total_plots = num_input_channels + num_output_channels + 2*num_output_channels
+    # (predicted) *2 (for raw and thresholded) + 1 (for combined predicted) + 1 (for combined predicted binary)
+    total_plots = num_input_channels + num_output_channels + 2*num_output_channels + 2
     logger.info(f"Evaluate: Total plots to make: {total_plots}")
     num_rows = total_plots // max_num_cols + int(total_plots % max_num_cols > 0)
     logger.info(f"Evaluate: Creating subplot with {num_rows} rows and {max_num_cols} columns.")
+    # Combine the predicted masks into one image for visualisation, don't include the background channel ofc
+    combined_predicted = np.sum(mask_predicted[:, :, 1:], axis=2)
+    # for the binary predicted mask, choose the value of the channel with the highest confidence per pixel
+    combined_predicted_binary = np.argmax(mask_predicted_binary[:, :, 1:], axis=2)
     fig, axes = plt.subplots(num_rows, min(total_plots, max_num_cols))
     plot_index = 0
     logger.info(
@@ -52,5 +56,14 @@ def plot_image_mask_prediction(
         ax.imshow(mask_predicted_binary[:, :, i], cmap="binary")
         ax.set_title(f"Pred bin Mask C{i}")
         plot_index += 1
+    # Plot combined predicted masks
+    ax = axes.flatten()[plot_index]
+    ax.imshow(combined_predicted, cmap="viridis")
+    ax.set_title("Combined Pred Mask")
+    plot_index += 1
+    ax = axes.flatten()[plot_index]
+    ax.imshow(combined_predicted_binary, cmap="viridis")
+    ax.set_title("Combined Pred bin Mask")
+    plot_index += 1
     plt.tight_layout()
     return fig
